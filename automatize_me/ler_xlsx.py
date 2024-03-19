fd9939806d054eb5a3542e6e240f76cd79e4ec62
ro.gon.cales <ro.go.calves@gmail.com>
2024-03-18 23:56:52 -0300

    Ajustando retorno de ler_xlsx importar

--- automatize_me/ler_xlsx.py
+++ automatize_me/ler_xlsx.py
@@ -5,7 +5,7 @@ import os, shutil
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-def importar(caminhoArquivo: str) -> dict[str, str]:
+def importar(caminhoArquivo: str) -> dict[str, list[str]]:
     """
     Importa a planilha
     
@@ -17,28 +17,35 @@ def importar(caminhoArquivo: str) -> dict[str, str]:
 
     Examples:
         >>> importar("")
-        {['erro': 'arquivo não informado']}
+        {'erro': ['arquivo não informado']}
 
         >>> importar('caminho/existe/arquivo.docx')
-        {'erro': 'formato não suportado, formato suportado .xlsx'}
+        {'erro': ['formato não suportado, formato suportado .xlsx']}
 
         >>> importar('caminho/nao/arquivo.xlsx')
-        {'erro': 'arquivo não encontrado'}
+        {'erro': ['arquivo não encontrado']}
 
         >>> importar('tests/resources/arquivos-teste/arquivo.xlsx')
-        {'sucesso': 'arquivo importado'}
+        {'sucesso': ['arquivo importado']}
     """
     if not caminhoArquivo:
-        return {'erro': ['arquivo não informado']}
+        return {'status': 'erro', 'mensagem': 'arquivo não informado'}
     
+    if not os.path.isfile(caminhoArquivo):
+        return {'status': 'erro', 'mensagem': 'arquivo não encontrado'}
+        
     _, extensao = os.path.splitext(caminhoArquivo)
     
     if extensao != ".xlsx":
         return {'status': 'erro', 'mensagem': 'formato não suportado, formato suportado .xlsx'}
     
-    if not os.path.isfile(caminhoArquivo):
-        return {'erro': ['arquivo não encontrado']}    
    
     shutil.copy(caminhoArquivo, os.path.join(DIR_PATH, 'resources', 'arquivos'))
-    return  {'sucesso': ['arquivo importado']}
+    return  {'status': 'sucesso', 'mensagem': 'arquivo importado'}
+
+
+def descrever() -> dict[str, list[dict[str, list[str]]]]:
+    """
+    Recupera o nome das planilhas contidas no arquivo, o nome dos campos da planilha
+    """
    
