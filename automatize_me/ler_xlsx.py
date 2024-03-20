03413bfb538332d1aeebc39dbdd926c4331cc48b
ro.go.calves <ro.go.calves@gmail.com>
2024-03-20 14:11:14 -0300

    Fatorando a ler_xlsx.py para trabalhar com a importação de arquivos da infrastructure

--- automatize_me/ler_xlsx.py
+++ automatize_me/ler_xlsx.py
@@ -1,9 +1,9 @@
 """
 Módulo responsável por abrir e recuperar dados de um XLSX
 """
-import os, shutil
+import os
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+from .insfrastructure.arquivo import importar_arquivo, validar_planilha
 
 def importar(caminhoArquivo: str) -> dict[str, str]:
     """
@@ -14,31 +14,7 @@ def importar(caminhoArquivo: str) -> dict[str, str]:
         
     Returns:
         Um dicionário contendo sucesso/erro e uma mensagem descritiva
-
-    Examples:
-        >>> importar("")
-        {'status': 'erro', 'mensagem': 'arquivo não informado'}
-
-        >>> importar('tests/resources/arquivos-teste/arquivo.docx')
-        {'status': 'erro', 'mensagem': 'formato não suportado, formato suportado .xlsx'}
-
-        >>> importar('caminho/nao/arquivo.xlsx')
-        {'status': 'erro', 'mensagem': 'arquivo não encontrado'}
-
-        >>> importar('tests/resources/arquivos-teste/arquivo.xlsx')
-        {'status': 'sucesso', 'mensagem': 'arquivo importado'}
+    
     """
-    if not caminhoArquivo:
-        return {'status': 'erro', 'mensagem': 'arquivo não informado'}
-    
-    if not os.path.isfile(caminhoArquivo):
-        return {'status': 'erro', 'mensagem': 'arquivo não encontrado'}
-        
-    _, extensao = os.path.splitext(caminhoArquivo)
-    
-    if extensao != ".xlsx":
-        return {'status': 'erro', 'mensagem': 'formato não suportado, formato suportado .xlsx'}
-    
-   
-    shutil.copy(caminhoArquivo, os.path.join(DIR_PATH, 'resources', 'arquivos'))
-    return  {'status': 'sucesso', 'mensagem': 'arquivo importado'}
+    validar_planilha(caminhoArquivo)
+    importar_arquivo(caminhoArquivo)
